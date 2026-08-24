2433b1e8c71d9a603073d5389d2edbdb79ce184a
adhclark-apps <adhclark@gmail.com>
2026-08-24 07:23:01 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,9 @@
-const CACHE = 'iron-function-v1';
+const CACHE = 'iron-function-v3';
 const ASSETS = [
   '/',
   '/index.html',
+  '/style.css',
+  '/app.js',
   '/manifest.json',
   'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=DM+Mono:wght@400;500&family=DM+Sans:wght@300;400;600;700&display=swap'
 ];
